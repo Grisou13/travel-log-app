fe8c80e759494a77634c0e985c84ce7a9266a4b1
Thomas Ricci <thomasricci11@gmail.com>
2023-06-27 11:22:50 +0200

    added place of interest toggle on map

--- src/app/dashboard/pages/trip-detail/trip-detail.component.ts
+++ src/app/dashboard/pages/trip-detail/trip-detail.component.ts
@@ -9,8 +9,10 @@ import {
   forkJoin,
   map,
   of,
+  pairwise,
   share,
   shareReplay,
+  startWith,
   switchMap,
   tap,
   withLatestFrom,
@@ -28,6 +30,35 @@ import * as _ from 'lodash';
 import { Place } from '../../models/places';
 import { FormArray, FormControl } from '@angular/forms';
 import { PlaceType } from '@httpClients/travelLogApi/places/schema';
+
+function distance(
+  first: GeoJSON.Point,
+  second: GeoJSON.Point,
+  unit: 'K' | 'N' = 'K'
+) {
+  const [lat1, lon1] = first.coordinates;
+  const [lat2, lon2] = second.coordinates;
+  var radlat1 = (Math.PI * lat1) / 180;
+  var radlat2 = (Math.PI * lat2) / 180;
+  var theta = lon1 - lon2;
+  var radtheta = (Math.PI * theta) / 180;
+  var dist =
+    Math.sin(radlat1) * Math.sin(radlat2) +
+    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  if (dist > 1) {
+    dist = 1;
+  }
+  dist = Math.acos(dist);
+  dist = (dist * 180) / Math.PI;
+  dist = dist * 60 * 1.1515;
+  if (unit == 'K') {
+    dist = dist * 1.609344;
+  }
+  if (unit == 'N') {
+    dist = dist * 0.8684;
+  }
+  return dist;
+}
 
 @Component({
   selector: 'app-trip-detail',
@@ -56,8 +87,11 @@ export class TripDetailComponent {
     }),
     shareReplay({ refCount: true, bufferSize: 1 })
   );
-  placesAsMarkers$ = this.places$.pipe(
-    map((x) => x.filter((y) => y.type === 'TripStop')),
+  tripStops$ = this.places$.pipe(
+    map((x) => x.filter((y) => y.type === 'TripStop'))
+  );
+
+  placesAsMarkers$ = this.tripStops$.pipe(
     map((x) => {
       return x.map((y) =>
         L.marker([y.location.coordinates[1], y.location.coordinates[0]], {
@@ -67,22 +101,63 @@ export class TripDetailComponent {
           .bindTooltip(y.name)
           .addEventListener('click', () => {
             console.log('CLicking on:', y);
-            this.selectedPlaceState.next(y);
+            const currentlySelected = this.selectedPlaceState.getValue();
+
+            if (currentlySelected === null) {
+              this.selectedPlaceState.next(y);
+              return;
+            }
+            if (currentlySelected.id === y.id) {
+              this.selectedPlaceState.next(null);
+              return;
+            }
+
+            //this.selectedPlaceState.next(y);
           })
       );
     })
   );
-  private selectedPlaceState = new BehaviorSubject<Place | null>(null);
-  selectedPlace$ = this.selectedPlaceState
-    .asObservable()
-    .pipe(filter((x) => !!x));
 
-  directions$ = this.places$.pipe(
-    switchMap((places) => {
-      if (places === null) return of([]);
-      const stops = places.filter((x) => x.type === 'TripStop');
+  private selectedPlaceState = new BehaviorSubject<Place | null>(null);
+  selectedPlace$ = this.selectedPlaceState.asObservable(); /*.pipe(
+    startWith(null),
+    pairwise(),
+    map(([previous, current]) => {
+      if (previous === null) return current;
+      if (current === null) return current;
+
+      if (previous.id === current.id) return null;
+      return current;
+    })
+  );*/
+  poi$ = combineLatest([this.selectedPlace$, this.places$]).pipe(
+    map(([selectedPlace, places]) => {
+      if (selectedPlace === null) return [];
+      return (
+        places.filter((x) => x.infos?.relatedToPlace === selectedPlace.id) || []
+      );
+    })
+  );
+  markers$: Observable<L.Marker[]> = combineLatest([
+    this.poi$.pipe(
+      map((pois) => {
+        if (!Array.isArray(pois)) return [];
+
+        return pois.map((x) =>
+          L.marker([x.location.coordinates[1], x.location.coordinates[0]], {
+            title: x.name,
+          }).bindTooltip(x.name)
+        );
+      })
+    ),
+    this.placesAsMarkers$,
+  ]).pipe(map(([pois, places]) => [...places, ...pois]));
+
+  directions$ = this.tripStops$.pipe(
+    switchMap((stops) => {
+      if (stops === null) return of([]);
       if (stops.length < 2) {
-        return of(null);
+        return of([]);
       }
       const waypoints = _.sortBy(stops, 'order').map(
         (s) => s.location.coordinates
@@ -120,6 +195,13 @@ export class TripDetailComponent {
               previous: {},
               next: {},
             },
+            infos: {
+              relatedToPlace:
+                this.placeType.value !== null &&
+                this.placeType.value === 'PlaceOfInterest'
+                  ? getPlaceClosest(places, $event)
+                  : '-1',
+            },
             location: $event.location,
           });
         })
@@ -130,4 +212,12 @@ export class TripDetailComponent {
         error: console.error,
       });
   }
+}
+function getPlaceClosest(places: Place[], $event: Result): string {
+  const distances = places.reduce((acc, cur) => {
+    acc.push({ id: cur.id, dist: distance($event.location, cur.location) });
+    return acc;
+  }, [] as { id: string; dist: number }[]);
+  const sorted = _.sortBy(distances, 'dist');
+  return _.first(sorted)?.id || '-1';
 }
