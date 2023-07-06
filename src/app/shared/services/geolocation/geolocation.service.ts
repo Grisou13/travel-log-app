4168588dea2f468f7c50421a7478efb7863cc6ec
Thomas Ricci <thomasricci11@gmail.com>
2023-07-06 08:56:23 +0000

    Added geolocate user:

--- src/app/shared/services/geolocation/geolocation.service.ts
+++ src/app/shared/services/geolocation/geolocation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 const browserHasApi = 'geolocation' in navigator;
 
@@ -17,5 +18,19 @@ export class GeolocationService {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     })
   };
-
+  getUserPosition(options: PositionOptions = {timeout:500, enableHighAccuracy:true}): Observable<GeolocationPosition> {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+          //console.log(`this user position: ${pos.coords.latitude}, ${pos.coords.longitude}`)
+        })
+      } else {
+        observer.error();
+      }
+    })
+  }
 }
