3aa0558dd2c0452edc95ea57983e33aab9eb2a20
Thomas Ricci <thomasricci11@gmail.com>
2023-07-06 19:55:52 +0200

    improved readability of highlight directive

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -23,26 +23,22 @@ export class HighlightDirective implements OnChanges {
   constructor(private el: ElementRef, private sanitizer: DomSanitizer) {}
 
   ngOnChanges(changes: SimpleChanges) {
-    if (this.el?.nativeElement) {
-      if ('searchTerm' in changes || 'caseSensitive' in changes) {
-        const text = (this.el.nativeElement as HTMLElement).textContent || '';
-        if (this.searchTerm === '') {
-          this.content = text;
-        } else {
-          const regex = new RegExp(
-            this.searchTerm,
-            this.caseSensitive ? 'g' : 'gi'
-          );
-          const newText = text.replace(regex, (match: string) => {
-            return `<mark class="highlight ${this.customClasses}">${match}</mark>`;
-          });
-          const sanitzed = this.sanitizer.sanitize(
-            SecurityContext.HTML,
-            newText
-          );
-          this.content = sanitzed || '';
-        }
-      }
+    if (!this.el?.nativeElement) {
+      return;
     }
+    if (!('searchTerm' in changes || 'caseSensitive' in changes)) {
+      return;
+    }
+    const text = (this.el.nativeElement as HTMLElement).textContent || '';
+    if (this.searchTerm === '') {
+      this.content = text;
+      return;
+    }
+    const regex = new RegExp(this.searchTerm, this.caseSensitive ? 'g' : 'gi');
+    const newText = text.replace(regex, (match: string) => {
+      return `<mark class="highlight ${this.customClasses}">${match}</mark>`;
+    });
+    const sanitzed = this.sanitizer.sanitize(SecurityContext.HTML, newText);
+    this.content = sanitzed || '';
   }
 }
