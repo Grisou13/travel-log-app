6dd8c6352d614f222b08b30ec82c7904e939383f
Thomas Ricci <thomasricci11@gmail.com>
2023-07-19 01:00:48 +0200

    added animation to offcanvas: not working yet

--- src/app/shared/components/off-canvas/off-canvas.component.ts
+++ src/app/shared/components/off-canvas/off-canvas.component.ts
@@ -1,5 +1,13 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { Offcanvas, Ripple, Dropdown, initTE } from 'tw-elements';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Offcanvas, Ripple, Dropdown, Animate, initTE } from 'tw-elements';
 
 @Component({
   selector: 'app-off-canvas',
@@ -7,22 +15,24 @@ import { Offcanvas, Ripple, Dropdown, initTE } from 'tw-elements';
   styleUrls: ['./off-canvas.component.sass'],
 })
 export class OffCanvasComponent implements AfterViewInit {
-  @ViewChild("sidebar") sidebar!: ElementRef<HTMLDivElement>;
-  @Output() showChanged = new EventEmitter<boolean>()
+  @ViewChild('sidebar') sidebar!: ElementRef<HTMLDivElement>;
+  @ViewChild('backdrop') backdrop!: ElementRef<HTMLDivElement>;
+  @Output() showChanged = new EventEmitter<boolean>();
 
   constructor() {
-    initTE({ Offcanvas, Ripple, Dropdown });
+    initTE({ Offcanvas, Ripple, Dropdown, Animate });
   }
-  
-  onCloseRequested(){
-    this.sidebar?.nativeElement.removeAttribute('data-te-offcanvas-show')
+
+  onCloseRequested() {
+    this.sidebar?.nativeElement.removeAttribute('data-te-offcanvas-show');
+    this.backdrop?.nativeElement.classList.add('opacity-0');
     this.showChanged.emit(false);
   }
-  
+
   ngAfterViewInit(): void {
-    
+    this.backdrop?.nativeElement.classList.add('opacity-0');
+
     this.sidebar?.nativeElement.setAttribute('data-te-offcanvas-show', 'true');
     // data-te-offcanvas-show
   }
-  
 }
