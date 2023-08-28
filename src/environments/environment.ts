5ca5370a7aa4037018a7d6ce8238b730dff9c214
Thomas Ricci <thomasricci11@gmail.com>
2023-08-29 00:29:59 +0200

    remove old ors key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   travelLogApi: 'https://trek-traks-api.onrender.com/api',
   openRouteServiceApiKey:
-    '5b3ce3597851110001cf6248628365216b2f4cf290c9014875c17180',
+    '5b3ce3597851110001cf62481c84cc5bde5248958a4254bf33f6f6aa',
   openRouteUrl: 'https://api.openrouteservice.org/',
 };
