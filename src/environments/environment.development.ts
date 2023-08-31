70a0b830f0afa754b7d371632ad2a53f403a2073
Thomas Ricci <thomasricci11@gmail.com>
2023-08-31 16:23:56 +0200

    remove env

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  travelLogApi: 'http://localhost:3000/api', //TODO move to localhost
-  openRouteServiceApiKey:
-    '5b3ce3597851110001cf62481c84cc5bde5248958a4254bf33f6f6aa',
-  //'5b3ce3597851110001cf6248628365216b2f4cf290c9014875c17180', //personnal key
-  openRouteUrl: 'https://api.openrouteservice.org/',
-};
